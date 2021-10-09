b43e6ffc3824ed2b8ddd25ec6d32262ca34b87c7
İsmail ASLAN <1aslan1ismail@gmail.com>
2021-10-09 15:21:52 +0300

    end of lesson1

--- Lesson1/src/clarusway/urls.py
+++ Lesson1/src/clarusway/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('home/', home),
-    path('fs',include('fscohort.urls'))
+    path('fs/',include('fscohort.urls'))
 ]
