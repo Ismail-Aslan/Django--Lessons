95bf3fceb66315fe34340d0df5d8fb52f8e39937
İsmail ASLAN <1aslan1ismail@gmail.com>
2021-10-14 22:33:03 +0300

    .

--- Lesson5/student/views.py
+++ Lesson5/student/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from student.models import Student
 from .forms import StudentForm
+from django.shortcuts import redirect, render
+
 
 def index(request):
     return render(request, 'student/index.html')
 
+
 def student_page(request):
     form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid(): # eğer formda zorunlu kısımlar düzgün doldurulmuşsa
+            form.save()
+            return redirect('student') # formu kaydettikten sonra urlsde name='student' demiştik oraya yönlendirdi
     context = {
         'form': form
     }
-    return render(request,'student/student.html', context)
+    return render(request, 'student/student.html', context)
