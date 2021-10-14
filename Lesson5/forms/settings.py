9b316f953792dd9ee0c04745948f66fdd9bca387
İsmail ASLAN <1aslan1ismail@gmail.com>
2021-10-14 22:49:30 +0300

    crispy_forms eklemek

--- Lesson5/forms/settings.py
+++ Lesson5/forms/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'student',
+    'crispy_forms'
     
 ]
 
@@ -131,3 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
