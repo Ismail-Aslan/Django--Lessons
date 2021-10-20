90a488047495d6dc90c1b7516600c01e7d232de8
İsmail ASLAN <1aslan1ismail@gmail.com>
2021-10-20 21:05:45 +0300

    .

--- Lesson7-To_Do_App/todo/views.py
+++ Lesson7-To_Do_App/todo/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-
+from .models import Todo
+from .forms import TodoAddForm
 
 def home(request):
     return render(request, "todo/home.html")
+
+
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos' : todos
+    }
+    return render(request, "todo/todo_list.html", context)
+
+
+def todo_add(request):
+    form = TodoAddForm()
+    
+    if request.method == "POST":
+        form = TodoAddForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {
+        'form' : form
+    }
+    return render(request, "todo/todo_add.html", context)
