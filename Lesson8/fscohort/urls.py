2d3b8cfe3ca493f7edceaabbe9569dead62e3f36
İsmail ASLAN <1aslan1ismail@gmail.com>
2021-10-21 20:34:12 +0300

    using templateView in views

--- Lesson8/fscohort/urls.py
+++ Lesson8/fscohort/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import home,student_list, student_add, student_detail, student_update,student_delete
 from django.views.generic import TemplateView
+from .views import HomeView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name = 'fscohort/home.html'), name="home"),
+    # path('',home, name = "home")
+    # path('', TemplateView.as_view(template_name = 'fscohort/home.html'), name="home"),
+    path('', HomeView.as_view(), name="home"),
     path('student_list/', student_list, name="list"),
     path('student_add/', student_add, name="add"),
     path('detail/<int:id>/', student_detail, name="detail"),
