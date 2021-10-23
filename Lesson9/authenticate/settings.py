52c774f2e031a6fc86b65789be238760773bbfb3
İsmail ASLAN <1aslan1ismail@gmail.com>
2021-10-23 11:35:30 +0300

    .

--- Lesson9/authenticate/settings.py
+++ Lesson9/authenticate/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
     'user_example.apps.UserExampleConfig',
 ]
 
