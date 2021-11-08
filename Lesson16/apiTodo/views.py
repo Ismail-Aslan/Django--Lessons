8967cfba1b2b4e3f1a4f02279befb7ed0d963cd0
İsmail ASLAN <1aslan1ismail@gmail.com>
2021-11-08 21:11:36 +0300

    first model created

--- Lesson16/apiTodo/views.py
+++ Lesson16/apiTodo/views.py
@@ -1,6 +1,5 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
-def home (request):
-    return HttpResponse()
+def home(request):
+    return HttpResponse('<center><h1>Welcome to apiTodo</h1></center>')
