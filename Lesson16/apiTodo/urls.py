5d050b5604ea8b3d9678a105422f245991229e92
İsmail ASLAN <1aslan1ismail@gmail.com>
2021-11-11 20:20:07 +0300

    .

--- Lesson16/apiTodo/urls.py
+++ Lesson16/apiTodo/urls.py
@@ -1,8 +1,12 @@
 from django.urls import  path
-from .views import home,todoList,todoListCreate
+from .views import home,todoList,todoListCreate,todoListAll, todoListUpdate, todoListDelete,todoListDetail
 
 urlpatterns = [
     path('', home),
     path('todoList/', todoList),
     path('todoListCreate/', todoListCreate),
+    path('todoListAll/', todoListAll),
+    path('todoListUpdate/<int:pk>/', todoListUpdate),
+    path('todoListDelete/<int:pk>/', todoListDelete),
+    path('todoListDetail/<int:pk>/', todoListDetail),
 ]
--- Lesson16/apiTodo/views.py
+++ Lesson16/apiTodo/views.py
@@ -6,6 +6,7 @@ from rest_framework import serializers
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -27,3 +28,64 @@ def todoListCreate(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['GET','POST'])
+def todoListAll(request):
+    if request.method == "POST":
+        serializer = TodoSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    elif request.method == "GET":
+        queryset = Todo.objects.all()
+        serializer = TodoSerializer(queryset,many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+@api_view(['GET','PUT'])
+def todoListUpdate(request,pk):
+    if request.method == "PUT":
+        queryset = Todo.objects.get(id=pk)
+        serializer = TodoSerializer(instance = queryset, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)  
+      
+        
+    elif request.method == "GET":
+        queryset = Todo.objects.get(id=pk)
+        serializer = TodoSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+      
+@api_view(['DELETE'])
+def todoListDelete(request,pk):
+    if request.method == "DELETE":
+        queryset = Todo.objects.get(id=pk)
+        queryset.delete()
+        return Response("Item deleted",status=status.HTTP_200_OK)  
+    
+    
+@api_view(['GET','PUT','DELETE'])
+def todoListDetail(request,pk):
+    if request.method == "PUT":
+        queryset = Todo.objects.get(id=pk)
+        serializer = TodoSerializer(instance = queryset, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)  
+      
+        
+    elif request.method == "GET":
+        queryset = Todo.objects.get(id=pk)
+        serializer = TodoSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == "DELETE":
+        queryset = Todo.objects.get(id=pk)
+        queryset.delete()
+        return Response("Item deleted",status=status.HTTP_200_OK)  
+    
